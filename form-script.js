88705373d97c2512d65fa8a7238a482c15d93fd2
Sakthikarthick <sakthikarthick3107@gmail.com>
2024-02-14 08:54:02 +0530

    text field enablke

--- form-script.js
+++ form-script.js
@@ -2,18 +2,25 @@ let textBtn = document.getElementById('createText');
 
 textBtn.addEventListener('click' ,()=>{
     let p = document.createElement('input');
-    p.type = 'text';
-    p.placeholder = 'Placeholder';
+    let inputParameter = document.getElementById('inputParameter').value;
+    if(inputParameter){
+        p.type = 'text';
+        p.placeholder = inputParameter;
 
-    let rowFields = document.createElement('div');
-    rowFields.className = 'row-fields'
-    rowFields.appendChild(p);
-    let label = document.createElement('label');
-    label.innerHTML = 'TextInput';
+        let rowFields = document.createElement('div');
+        rowFields.className = 'row-fields'
+        rowFields.appendChild(p);
+        let label = document.createElement('label');
+        label.innerHTML = inputParameter;
 
-    let container = document.createElement('div');
-    container.className = 'input-container';
-    container.appendChild(label);
-    container.appendChild(rowFields)
-    document.getElementById('form').appendChild(container);
+        let container = document.createElement('div');
+        container.className = 'input-container';
+        container.appendChild(label);
+        container.appendChild(rowFields)
+        document.getElementById('form').appendChild(container);
+    }
+    else{
+        alert('Provide a label name for your new textfield')
+    }
+    
 })
